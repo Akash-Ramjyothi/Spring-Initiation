a8e701c1bb48c916669e8c5f0e5442467b68cee6
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-09-22 23:26:07 +0530

    Added initializer

---
+++ src/main/java/com/spring/initiation/InitiationApplication.java
@@ -0,0 +1,13 @@
+package com.spring.initiation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitiationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitiationApplication.class, args);
+	}
+
+}
