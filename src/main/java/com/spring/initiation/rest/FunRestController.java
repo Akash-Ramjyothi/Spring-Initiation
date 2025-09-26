7350b6b6d54d1fb75d4ecd09e88594eada7635d4
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-09-26 17:43:45 +0530

    Added Spring Dev Tools

--- src/main/java/com/spring/initiation/rest/FunRestController.java
+++ src/main/java/com/spring/initiation/rest/FunRestController.java
@@ -7,7 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
 
     @GetMapping("/")
-    String sayHello(){
+    public String sayHello(){
         return"Hello World.";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day.";
     }
 }
