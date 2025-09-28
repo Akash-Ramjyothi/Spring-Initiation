77ce945cc68631efb70bb72326516860ef7c8c68
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2025-09-28 20:54:01 +0530

    Used properties injection

--- src/main/java/com/spring/initiation/rest/FunRestController.java
+++ src/main/java/com/spring/initiation/rest/FunRestController.java
@@ -1,23 +1,35 @@
 package com.spring.initiation.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${coach.name}")
+    public String coachName;
+
+    @Value("${team.name}")
+    public String teamName;
+
     @GetMapping("/")
-    public String sayHello(){
-        return"Hello World.";
+    public String sayHello() {
+        return "Hello World.";
     }
 
     @GetMapping("/workout")
-    public String getDailyWorkout(){
+    public String getDailyWorkout() {
         return "Run a hard 5k";
     }
 
     @GetMapping("/fortune")
-    public String getDailyFortune(){
+    public String getDailyFortune() {
         return "Today is your lucky day.";
+    }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach Name: " + coachName + " Team Name: " + teamName;
     }
 }
